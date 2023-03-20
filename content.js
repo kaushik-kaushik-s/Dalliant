afc753a9f0b1daaabf0231470bb212217347a819
kaushik-sannidhi <82121002+kaushik-sannidhi@users.noreply.github.com>
2023-03-20 13:54:21 -0400

    Create content.js

---
+++ content.js
@@ -0,0 +1,39 @@
+const hereco = async (url) => {
+    const cohereApiKey = 'key_here';//add ur cohere api key here
+    const cohereEndpointUrl = 'https://api.cohere.ai/classify';
+
+    const cohereReq = {
+        model: 'fb532845-9e54-40cb-bdfa-20ab37c36ef5-ft',
+        inputs: [url]
+    };
+
+    const reqBody = JSON.stringify(cohereReq);
+
+    const response = await fetch(cohereEndpointUrl, {
+        method: 'POST',
+        headers: {
+            'Authorization': `Bearer ${cohereApiKey}`,
+            'Content-Type': 'application/json'
+        },
+        body: reqBody
+    });
+
+    const responseBody = await response.json();
+    return responseBody.classifications[0].prediction;
+};
+
+const blockedGenres = ["Shopping"];
+
+window.addEventListener('load', async () => {
+    const url = window.location.href;
+    const genre = await hereco(url);
+    if (blockedGenres.includes(genre)) {
+        document.querySelector('body').innerHTML = `
+      <div style="position: fixed; top: 0; left: 0; width: 100%; height: 100%; background: #881fe1; z-index: 999999;">
+        <p style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); font-size: 66px; font-weight: bold; color: #fff; text-align: center;">
+          The website is blocked!
+        </p>
+      </div>
+    `;
+    }
+});
